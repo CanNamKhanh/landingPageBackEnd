90ba8ef8632bff70130304077eb88de4f7efc0d5
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-04 04:57:56 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://rosie-boost.vercel.app/"],
+    origin: ["http://localhost:5173", "https://rosie-boost.vercel.app"],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
   }),
