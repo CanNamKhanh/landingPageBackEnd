b7b8151fd546fc4d242fcbb3fb079cc87f888345
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-04 14:30:58 +0700

    update3

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,17 @@
 import express from "express";
-import cors from "cors";
 import formRoute from "./routes/form.route";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://rosie-boost.vercel.app",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  }),
-);
-
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
+  const origin = req.headers.origin || "*";
 
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
+  res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
+    return res.status(204).end();
   }
 
   next();
@@ -32,7 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use("/api", formRoute);
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("OK");
 });
 
