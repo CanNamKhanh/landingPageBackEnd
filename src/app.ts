3513c9649a8f4c9eb0ced106bcf03b02b9a514e1
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-04 14:27:37 +0700

    update2

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,22 @@ app.use(
   }),
 );
 
-// ⭐ BẮT BUỘC
-app.options("*", cors());
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use("/api", formRoute);
