67e7def9a3819c6d04b2fc81833d215c1ccf787a
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-02-04 14:12:04 +0700

    update2

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import formRoute from "./routes/form.route";
 
 const app = express();
+
 app.use(
   cors({
     origin: "https://rosie-boost.vercel.app",
@@ -11,13 +12,14 @@ app.use(
   }),
 );
 
+// ⭐ BẮT BUỘC
+app.options("*", cors());
+
 app.use(express.json());
 app.use("/api", formRoute);
 
 app.get("/", (req, res) => {
   res.send("OK");
 });
-
-app.options("*", cors());
 
 export default app;
